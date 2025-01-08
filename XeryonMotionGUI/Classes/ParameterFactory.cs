a1f563a3dfb0d500eb68e602ad4bb41e658b23b4
Thomas Vanden Driesch <thomas.vandendriesch@xeryon.com>
2025-01-08 01:32:55 +0100

    Refactor Axis class and update version
    Cleaned up using directives in Axis.cs. Refactored Parameters collection initialization to use ParameterFactory.CreateParameters(). Changed Parameters property to a public getter-only property. Improved property set accessors for readability. Added PositionReachedElapsedTime property and private field _positionReachedLastFalseTime. Updated Version attribute in Package.appxmanifest to 1.1.5.0. Refactored OnFilePickerButtonClick method in ParametersPage.xaml.cs. Added ParameterFactory class for better code organization.

---
+++ XeryonMotionGUI/Classes/ParameterFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeryonMotionGUI.Classes;
+public static class ParameterFactory
+{
+    public static ObservableCollection<Parameter> CreateParameters()
+    {
+        return new ObservableCollection<Parameter>
+            {
+                new(0, 1, 0.01, 0.01, "Zone 1 Size:"),
+                new(0, 1, 0.01, 0.1, "Zone 2 Size:"),
+                new (0, 185000, 1000, 85000, "Zone 1 Frequency:"),
+                new (0, 185000, 1000, 83000, "Zone 2 Frequency:"),
+                new (0, 200, 5, 90, "Zone 1 Proportional:"),
+                new (0, 200, 5, 45, "Zone 2 Proportional:"),
+                new (0, 200, 2, 4, "Position Tolerance:"),
+                new (0, 400, 5, 200, "Speed:"),
+                new (0, 64400, 1000, 32000, "Acceleration:"),
+                new (0, 1500, 100, 0, "Mass:"),
+                new (0, 1, 1, 1, "Amplitude Control:"),
+                new (-200, 0, 1, -100, "Left Soft Limit:"),
+                new (0, 200, 1, 100, "Right Soft Limit:"),
+                new (0, 1, 1, 1, "Phase Correction:"),
+                new (0, 1000, 1, 50, "Error Limit:")
+            };
+    }
+}
